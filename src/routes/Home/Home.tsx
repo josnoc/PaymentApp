f0e398682c54ab7f40aaf569ed4873b7a4887fe8
Jose Enoc Martinez R <josnocpp@gmail.com>
2020-04-23 11:33:43 -0500

    Finished Home Component.

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,16 +1,37 @@
 import * as React from "react";
+import Moment from "moment";
 
 import "./Home.scss";
+import PaymentList from "../../components/PaymentList";
+import Total from "../../components/Total";
+import Add from "../../components/Add";
+import AppStore, { initialState } from "../../store/app";
+import PaymentVisualizer from "../../components/PaymentVisualizer";
+import ContextMenu from "../../components/ContextMenu";
 
 const Home: React.SFC = () => {
+  const [appState, setAppState] = React.useState(initialState);
+
+  React.useLayoutEffect(() => {
+    const subscription = AppStore.subscribe(setAppState);
+    AppStore.init();
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  });
+
   return (
     <div className="home">
-      <h1 className="title">Welcome to a new React App!</h1>
-      <img
-        src={require("../../assets/images/React-icon.svg")}
-        alt="react icon"
-        className="react-logo"
-      />
+      <Total />
+      <PaymentList />
+      <Add />
+      {appState.isPaymentVisualizerOpened && (
+        <PaymentVisualizer
+          mode={appState.visualizerOpenMode}
+          id={appState.visualizerId}
+        />
+      )}
     </div>
   );
 };
