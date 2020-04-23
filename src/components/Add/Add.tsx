73ea94155ee993e1fec8b80c29566439a9766875
Jose Enoc Martinez R <josnocpp@gmail.com>
2020-04-23 08:10:35 -0500

    Hooked Add component to store.

--- src/components/Add/Add.tsx
+++ src/components/Add/Add.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import AppStore from "../../store/app";
 import "./Add.scss";
 
 const Add = () => {
-  return <p className="Add icon-add-circle"></p>;
+  const onClick = () => {
+    console.log("working?");
+    AppStore.openNewPayment();
+  };
+
+  return <p onClick={onClick} className="Add icon-add-circle"></p>;
 };
 
 export default Add;
