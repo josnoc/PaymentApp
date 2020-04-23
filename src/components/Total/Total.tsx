bdf410b8dfa6a084d7aab4d64121ae6a0f212987
Jose Enoc Martinez R <josnocpp@gmail.com>
2020-04-23 11:31:24 -0500

    Hoked Total component to Payment Store.

--- src/components/Total/Total.tsx
+++ src/components/Total/Total.tsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useLayoutEffect } from "react";
 
 import TotalView from "./Total.view";
+import PaymentStore, { initialState } from "../../store/payments";
 
 const Total = () => {
-  return <TotalView total={1689.47} />;
+  const [paymentState, setPaymentState] = useState(initialState);
+  const [total, setTotal] = useState(0);
+
+  useLayoutEffect(() => {
+    const subscription = PaymentStore.subscribe(setPaymentState);
+    PaymentStore.init();
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  });
+
+  const calculateTotal = () => {
+    let sum = 0;
+    paymentState.forEach((payment) => (sum += payment.amount));
+    return sum;
+  };
+
+  return <TotalView total={calculateTotal()} />;
 };
 
 export default Total;
